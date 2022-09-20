6d47e8ef0b2c18ffa62e9a8128ed299d45dc9153
Vladislav Fomenko <mishyak.as@gmail.com>
2022-09-20 07:01:09 +0300

    feat: add aimation to marquee

--- pages/main/index.js
+++ pages/main/index.js
@@ -1,4 +1,29 @@
 window.onload = function(){
-  const footer = document.querySelector(".footer");
-  TweenLite.from(footer, 1, {left:"632px"});
+  initMarquee(6);
+}
+
+function initMarquee(time) {
+  const marqueeElements = document.querySelectorAll('.marquee-element');
+  const arrayOfMarqueeElements = gsap.utils.toArray(marqueeElements);
+  const widthsOfMarqueeElements = arrayOfMarqueeElements.map((el) => parseInt(window.getComputedStyle(el).width));
+  const maxLengthOfMarqueeElement = Math.max(...widthsOfMarqueeElements);
+  const lengthOfLastMarqueeElement = widthsOfMarqueeElements.slice(-1)[0];
+  const gap = parseFloat(window.getComputedStyle(marqueeElements[0].parentNode).gap);
+  let shift = 0;
+
+  gsap.set(marqueeElements, {
+    x: (i) => {
+      return marqueeElements[i - 1] ? shift += (marqueeElements[i - 1].offsetWidth) + gap : shift += lengthOfLastMarqueeElement + gap;
+    }
+  });
+
+  gsap.to(marqueeElements, {
+    duration: time,
+    ease: 'none',
+    x: '-=' + shift,
+    modifiers: {
+      x: gsap.utils.unitize((x) => parseFloat(x + shift) % shift - maxLengthOfMarqueeElement),
+    },
+    repeat: -1,
+  });
 }
